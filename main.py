64b6caf30b5393a9689369ab42b87f23376c38ff
Nils Jakobson Mo <nilsmo1@hotmail.com>
2021-05-16 12:47:42 +0200

    Small change in the creation of the board in the pygame window. Also a file for colors has been added.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 from Board import Board
 from Piece import Piece
+from Colors import BLACK, WHITE
 import pygame
+
+def display_board(window: pygame.Surface, board: Board) -> None:
+    pass 
 
 def print_board(board: Board) -> None:
     """
@@ -25,22 +29,28 @@ def main() -> None:
     @ Return:
         None
     """
-    # Pygame init and window init
     pygame.init()
     window_size = (800,800)
     window = pygame.display.set_mode(window_size)
-    
+
     board = Board(11)
     board.init_board()
     print_board(board)
     
     Running = True
+    # Press Q or the close the window to exit
     while Running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                Running = False
+            if (event.type == pygame.QUIT or
+                event.type == pygame.KEYDOWN and
+                event.key  == pygame.K_q
+                ): Running = False
+
+        window.fill(WHITE)
+        display_board(window, board)        
+        pygame.display.update()
 
 
+if __name__ == "__main__":
+    main()
 
-
-main()
